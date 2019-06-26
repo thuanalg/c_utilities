cf215d3de46472b01bc994dce98046b241f46e5d
Thuan Nguyen <thuan.nguyen@innogram.co>
2019-06-26 11:25:09 +0700

    Open session, close session.

--- src/mycode.c
+++ src/mycode.c
@@ -8,63 +8,76 @@
 #include <fcntl.h> /* For O_* constants */
 #include <pthread.h>
 
-static int _X(init_service)(char *path);
-
-int _X(sqlite3_open)(char *path)
+static int _X(init_service)(char *path, void **, int);
+#define PRIVATE_NAME_UTV  "/5112052a-02a6-4818-ac42-2de914ef5700_"
+#define UTV_KHL_MODE      (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define UTV_KHL_SZ        (20 << 1)
+static void * data_shm_xyz = 0;
+static void * comm_shm_xyz  = 0;
+static char sta_shm_key[1024];
+/************************************************************************
+@return: session
+************************************************************************/
+int _X(xyz_close)(int session)
+{
+  int err = 0;
+  do
+  {
+    if(data_shm_xyz){
+      err = unmap(data_shm_xyz, UTV_KHL_SZ);
+    }
+    if(comm_shm_xyz){
+      err = unmap(comm_shm_xyz, UTV_KHL_SZ);
+    }
+  }
+  while(0);
+  return err;
+}
+int _X(xyz_open)(char *path)
 {
   int err;
   do
   {
-
+    err = _X(init_service)(path, &data_shm_xyz, UTV_KHL_SZ);
+    err = _X(init_service)(path, &comm_shm_xyz, UTV_KHL_SZ);
   }
   while(0);
 
   return err;
 }
 
-#define PRIVATE_NAME_STA  "/5112052a-02a6-4818-ac42-2de914ef5700_"
-#define STA_SHM_MODE      (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
-#define STA_SHM_SZ        (20 << 1)
-static void * sta_shm_sqlite3 = 0;
-static char sta_shm_key[1024];
 
-int _X(init_service)(char *path)
+int _X(init_service)(char *path, void **out, int sz)
 {
   char create = 0;
   char *key = sta_shm_key;
   int err = 0;
   int shm = 0;
   void *data = 0;
-  sprintf(key, "%s%s", PRIVATE_NAME_STA, key);
+  sprintf(key, "%s%s", PRIVATE_NAME_UTV, key);
   do{
-    shm = shm_open(key, O_CREAT|O_RDWR | O_EXCL, STA_SHM_MODE);  
+    shm = shm_open(key, O_CREAT|O_RDWR | O_EXCL, UTV_KHL_MODE);  
     if(shm >= 0)
     {
       create = 1;
     }
     else
     {
-      shm = shm_open(key, O_RDWR, STA_SHM_MODE);  
+      shm = shm_open(key, O_RDWR, UTV_KHL_MODE);  
       if(shm < 0)
       {
-        //err = CANNOT_INIT_SHM;
         err = __LINE__;
         break;
       }
     }
-    err = ftruncate(shm, STA_SHM_SZ);
-    if(err)
-    {
-      break;
-    }
-    err = close(shm); 
-    if(err)
-    {
-      break;
-    }
-    sta_shm_sqlite3 = mmap(0, STA_SHM_SZ, 
+//    err = ftruncate(shm, UTV_KHL_SZ);
+//    if(err)
+//    {
+//      break;
+//    }
+    (*out) = mmap(0, sz, 
         PROT_READ | PROT_WRITE | PROT_EXEC , MAP_SHARED, shm, 0);
-    if(!sta_shm_sqlite3)
+    if(!(*out))
     {
       err = __LINE__;
       break;
@@ -73,9 +86,12 @@ int _X(init_service)(char *path)
     {
       break;
     }
-    memset(sta_shm_sqlite3, 0, STA_SHM_SZ);
+    memset((*out), 0, UTV_KHL_SZ);
   }
   while(0);
-
+  if(shm > -1)
+  {
+    err = close(shm); 
+  }
   return err;
 }
